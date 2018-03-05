2156ce5eb28a848346d21b722948d45618c105e4
Mondhs <mondhs@gmail.com>
2018-03-05 20:51:25 +0200

    added command history list. Improved UI. Support for 4.3 android.

--- app/src/main/java/io/github/mondhs/poliepa/MainActivity.kt
+++ app/src/main/java/io/github/mondhs/poliepa/MainActivity.kt
@@ -5,8 +5,11 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.widget.ArrayAdapter
 import edu.cmu.pocketsphinx.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.longToast
@@ -16,14 +19,13 @@ import java.io.IOException
 import java.lang.Exception
 
 
+/* Named searches allow to quickly reconfigure the decoder */
+private const val LIEPA_CMD = "liepa_commands"
+
+/* Used to handle permission request */
+private const val PERMISSIONS_REQUEST_RECORD_AUDIO = 1
 
 class MainActivity : AppCompatActivity(),RecognitionListener {
-
-    /* Named searches allow to quickly reconfigure the decoder */
-    private val LIEPA_CMD = "liepa_commands"
-
-    /* Used to handle permission request */
-    private val PERMISSIONS_REQUEST_RECORD_AUDIO = 1
 
     private var recognizer: SpeechRecognizer? = null
 
@@ -31,11 +33,16 @@ class MainActivity : AppCompatActivity(),RecognitionListener {
 
     private val liepaHelper = LiepaRecognitionHelper()
 
+    private val resultArray = mutableListOf("")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        caption_text.text = "Ruošiamas atpažintuvas"
+        caption_text.setText("Ruošiamas atpažintuvas")
+
+        resultList.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, this.resultArray)
+
         val permissionCheck = ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.RECORD_AUDIO)
         if (permissionCheck != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), PERMISSIONS_REQUEST_RECORD_AUDIO)
@@ -47,7 +54,7 @@ class MainActivity : AppCompatActivity(),RecognitionListener {
         super.onResume()
         // Recognizer initialization is a time-consuming and it involves IO,
         // so we execute it in async task
-        doRecognition();
+        doRecognition()
     }
 
     private fun doRecognition() {
@@ -56,10 +63,10 @@ class MainActivity : AppCompatActivity(),RecognitionListener {
                 with(it) {
                     val assets = Assets(it)
                     val assetDir = assets.syncAssets()
-                    it.setupRecognizer(assetDir);
-                    it.switchSearch(LIEPA_CMD);
-                    var currentWord = liepaHelper.nextWord(liepaContext)
-                    caption_text.text = currentWord
+                    it.setupRecognizer(assetDir)
+                    it.switchSearch(LIEPA_CMD)
+                    val currentWord = liepaHelper.nextWord(liepaContext)
+                    caption_text.setText(currentWord)
                     liepaContext.isRecogntionActive =true
                     longToast("Liepa klauso")
                 }
@@ -75,7 +82,7 @@ class MainActivity : AppCompatActivity(),RecognitionListener {
                 .setDictionary(File(assetsDir, "liepa-lt-lt.dict"))
                 .recognizer
 
-        recognizer?.addListener(this);
+        recognizer?.addListener(this)
 
         // Create grammar-based search for digit recognition
         val liepaCommandFile = File(assetsDir, "liepa_commands.gram")
@@ -83,8 +90,6 @@ class MainActivity : AppCompatActivity(),RecognitionListener {
         //initialize recognition result tracking context
         liepaContext = liepaHelper.initConxet(liepaCommandFile.readText())
     }
-
-
 
 
 
@@ -100,7 +105,7 @@ class MainActivity : AppCompatActivity(),RecognitionListener {
 
     private fun shutdownRecognition() {
         recognizer?.let {
-            it.cancel();
+            it.cancel()
             it.shutdown()
             liepaContext.isRecogntionActive = false
             longToast("Liepa nebegirdi")
@@ -118,56 +123,66 @@ class MainActivity : AppCompatActivity(),RecognitionListener {
                 // so we execute it in async task
                 doRecognition()
             } else {
-                finish();
+                finish()
             }
         }
     }
 
     private fun switchSearch(searchName: String) {
+        recordIndication.setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_ATOP)
         recognizer?.stop()
         recognizer?.startListening(searchName, 10000)
-        var currentWord = liepaHelper.nextWord(liepaContext)
-        caption_text.text = currentWord
+        val currentWord = liepaHelper.nextWord(liepaContext)
+        caption_text.setText(currentWord)
     }
 
     /*************************
      * RecognitionListener
      */
     override fun onResult(hypothesis: Hypothesis?) {
-        result_text.text = ""
+        result_text.setText("")
         hypothesis?.let {
 
             val correctCmdRatio:Int = liepaHelper.checkRecognition(liepaContext, it.hypstr)
 
-            val outText = "%s - %s \n\t tikimybė: %d \n\t geriausias balas: %d\n Bendras rezultastas %d(%d/%d)"
-                    .format(it.hypstr, liepaContext.previousWord, it.prob, it.bestScore, correctCmdRatio, liepaContext.correctCommands, liepaContext.testedCommands )
+            val outText = "%s = %s (tikimybė: %d; geriausias balas: %d)\n"
+                    .format(it.hypstr, liepaContext.previousWord, it.prob, it.bestScore )
 
             recognitionResultVIew.progress = correctCmdRatio
-            longToast(outText)
+            resultStat.setText("%d/%d".format(liepaContext.correctCommands, liepaContext.testedCommands))
+            this.resultArray.add(0, outText)
+            val adapter =  resultList.adapter
+            if (adapter is ArrayAdapter<*>) {
+                adapter.notifyDataSetChanged()
+            }
         }
     }
 
     override fun onPartialResult(hypothesis: Hypothesis?) {
         hypothesis?.let{
-            result_text.text = "%s".format(it.hypstr)
+            result_text.setText(it.hypstr)
         }
 
     }
 
     override fun onTimeout() {
-        switchSearch(LIEPA_CMD);
+        switchSearch(LIEPA_CMD)
     }
 
     override fun onBeginningOfSpeech() {
+        recordIndication.setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP)
     }
 
     override fun onEndOfSpeech() {
-        switchSearch(LIEPA_CMD);
+        switchSearch(LIEPA_CMD)
+
     }
 
     override fun onError(error: Exception?) {
-        caption_text.text = error?.message
+        caption_text.setText(error?.message)
     }
 
 
 }
+
+
